4e8c1d69e946a4742c754b50bfc5c4f0de80fed3
Jonatan Werpers <jonatan.werpers@it.uu.se>
2022-01-27 13:28:15 +0100

    Start adding a simple pde solver

---
+++ main.jl
@@ -0,0 +1,38 @@
+using GLMakie
+
+function periodic_grid(xlim, N)
+    Δx = (xlim[2]-xlim[1])/(N+1)
+    return range(0, step=Δx, length=N), Δx
+end
+
+function leap_frog!(vₙ₊₁, vₙ, vₙ₋₁, f, k)
+    for i ∈ eachindex(vₙ)
+        vₙ₊₁[i] = 2vₙ[i] - vₙ₋₁[i] + f(vₙ, i)
+    end
+end
+
+function D2(v, i, h)
+    vᵢ₋₁ = v[mod1(i-1,N)]
+    vᵢ   = v[i]
+    vᵢ₊₁ = v[mod1(i+1,N)]
+
+    return (vᵢ₋₁ - 2vᵢ + vᵢ₊₁)/h^2
+end
+
+N = 100
+xlim = (0,1)
+x, Δx = periodic_grid(xlim,N)
+
+v₀  = map(x->sin(x/2π), x)
+vₜ₀ = map(x->0,         x)
+
+k = h/2
+
+# First step with euler forward
+v₁ = v₀ + k * vₜ₀
+
+
+# function step!(vₙ₊₁, vₙ, vₙ₋₁)
+#     leap_frog!(vₙ₊₁, vₙ, vₙ₋₁)
+
+# end
