96a869488b19fe744fcf94d2df54e4f930088f54
Jonatan Werpers <jonatan@werpers.com>
2022-01-28 07:50:48 +0100

    Start setting up figure

--- main.jl
+++ main.jl
@@ -5,13 +5,34 @@ function main()
     xlim = (0,1)
     x, Δx = periodic_grid(xlim,N)
 
-    v₀  = map(x->sin(x/2π), x)
+    v₀  = map(x->sin(2π*x), x)
     vₜ₀ = map(x->0,         x)
 
     Δt = Δx/2
 
     # First step with euler forward
     v₁ = v₀ + Δt * vₜ₀
+
+    function step!(vₙ, vₙ₋₁, w)
+        leap_frog!(w, vₙ, vₙ₋₁, (v,i)->D2(v,i,Δx), Δt)
+        vₙ, vₙ₋₁, w = w, vₙ, vₙ₋₁
+    end
+
+    fig = Figure()
+
+    ax_v = Axis(fig[1,1])
+    ax_vₜ = Axis(fig[2,1])
+
+    @show x
+    @show v₀
+    lines!(ax_v, x, v₀;
+        label="v",
+    )
+    lines!(ax_vₜ, x, vₜ₀;
+        label="vₜ",
+    )
+
+    display(fig)
 end
 
 
@@ -37,10 +58,6 @@ function D2(v, i, Δx)
     return (vᵢ₋₁ - 2v[i] + vᵢ₊₁)/Δx^2
 end
 
-function step!(vₙ, vₙ₋₁, w)
-    leap_frog!(w, vₙ, vₙ₋₁, (v,i)->D2(v,i,Δx), Δt)
-    vₙ, vₙ₋₁, w = w, vₙ, vₙ₋₁
-end
 
 function prompt(msg = "Press [enter] to continue")
     print(msg, " ")
