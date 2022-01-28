964b4d8bb81303ec4fe1a0b54bd2163f9d2c16b0
Jonatan Werpers <jonatan@werpers.com>
2022-01-28 08:34:06 +0100

    Add time derivative to plot

--- main.jl
+++ main.jl
@@ -8,7 +8,7 @@ function main()
     v₀  = map(x->sin(2π*x), x)
     vₜ₀ = map(x->0,         x)
 
-    Δt = Δx/2
+    Δt = Δx
 
     # First step with euler forward
     v₁ = v₀ + Δt * vₜ₀
@@ -21,13 +21,17 @@ function main()
 
     # Observables for plotting
     v = Observable(vₙ)
+    vₜ = Observable(copy(vₙ))
 
     "Take simulation step and update plot state"
     function step!()
         leap_frog!(w, vₙ, vₙ₋₁, (v,i)->D2(v,i,Δx), Δt)
+        map!((vₙ₊₁,vₙ₋₁)-> (vₙ₊₁-vₙ₋₁)/2Δt, vₜ.val, w, vₙ₋₁)
+
         vₙ, vₙ₋₁, w = w, vₙ, vₙ₋₁
 
         v[] = vₙ
+        notify(vₜ)
     end
 
 
@@ -39,6 +43,9 @@ function main()
     scatter!(ax, x, v;
         label="v",
     )
+    scatter!(ax, x, vₜ;
+        label="vₜ",
+    )
 
     display(fig)
 
@@ -47,8 +54,6 @@ function main()
         sleep(1/24)
     end
 end
-
-
 
 function periodic_grid(xlim, N)
     Δx = (xlim[2]-xlim[1])/N
@@ -93,6 +98,11 @@ function D1(v, i, Δx)
     return (vᵢ₊₁ - vᵢ₋₁)/2Δx
 end
 
+function D1!(vₓ, v, Δx)
+    for i ∈ eachindex(v)
+        vₓ[i] = D1(v,i,Δx)
+    end
+end
 
 function prompt(msg = "Press [enter] to continue")
     print(msg, " ")
